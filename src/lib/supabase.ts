eff4eb81310ef3f6131fced6b2429eef8a024200
Haytam aatita(Niperia) <haytamaatita2001@gmail.com>
2025-01-17 22:04:45 +0100

    2af Checked

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,13 +1,11 @@
-import { createClient } from '@supabase/supabase-js';
+import { createClient } from "@supabase/supabase-js";
 
 const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
 const supabaseKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
 
 if (!supabaseUrl || !supabaseKey) {
-  throw new Error('Missing Supabase environment variables');
+  throw new Error("Missing Supabase environment variables");
 }
 
 export const supabase = createClient(supabaseUrl, supabaseKey);
-export const APP_URL = import.meta.env.PROD
-    ? 'https://auth-for-ocrx.vercel.app/'  // Replace with your actual Vercel domain
-    : 'http://localhost:5173';
+export const APP_URL = supabaseUrl;
